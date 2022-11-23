7867b8d71d9efd729fb47c08bb9f7c6a5ddfc4c5
CoolPuppyKid <68360803+CoolPuppyKid@users.noreply.github.com>
2022-11-22 16:33:52 -0800

    Initalcommit

---
+++ OrangeEngine/src/Orange.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// for use by Orange Apps
+
+#include <stdio.h>
+#include "Orange/Application.h"
+
+// ---Entry Point-----------------------
+#include "Orange/EntryPoint.h"
+// -------------------------------------
